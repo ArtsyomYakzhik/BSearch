52b74670a18995981bbe0dcd5ffe23251347a084
Nonamus-+ <yakhzhik@gmail.com>
2019-09-25 15:31:38 +0300

    Add bsearch function

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -10,8 +10,12 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(bsearch(new int[] { 16, 10, 7, 5, 5}, 5));
         }
 
-        static 
+        static int bsearch(int[] sortedArray, int X)
+        {
+            return sortedArray.FirstOrDefault(element => element < X);
+        }
     }
 }
